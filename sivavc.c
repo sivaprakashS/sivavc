b14f469a788fad0e1d9a2cb23aa2b1ba75e35b79
sivaprakashS <31438994+sivaprakashS@users.noreply.github.com>
2018-01-31 02:43:58 -0800

    sivavc.c

---
+++ sivavc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    char b;
+    printf("enter a character:");
+    scanf("%d",&d);
+    if(b=='a'||b=='e'||d=='i'||d=='o'||d=='u'||d=='A'||d=='E'||d=='I'||d=='O'||d=='U')
+    printf("%d is vowel",b);
+    else
+    printf("%d is consonant",d);
+} 
